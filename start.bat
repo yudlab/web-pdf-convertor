0765b4ca04d5e30952709619512ace8ff27624ce
yudlab <47621851+yudlab@users.noreply.github.com>
2023-03-07 23:01:23 +0000

    xs

--- start.bat
+++ start.bat
@@ -1,5 +1,16 @@
 @echo off
-echo Checking for updates
-git fetch && git pull
-cls
+title Checking for updates
+echo Checking for updates...
+echo ...
+echo Waiting for network availability...
+
+ping -n 10 www.github.com >nul
+if %errorlevel%==0 (
+    git fetch && git pull
+) else (
+    REM Internet is not accessible, do something else here
+    echo Internet is not accessible.
+    echo Update skipped.
+)
+
 node ./bin/www
