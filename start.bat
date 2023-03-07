ed17d6995f07a8b512cd19ed18d9cbf9e319f544
yudlab <47621851+yudlab@users.noreply.github.com>
2023-03-07 23:17:18 +0000

    testing auto update feature

--- start.bat
+++ start.bat
@@ -4,12 +4,19 @@ echo Checking for updates...
 echo ...
 echo Waiting for network availability...
 
-ping -n 10 www.github.com >nul
+ping -n 4 www.github.com >nul
 if %errorlevel%==0 (
-    git fetch && git pull
-    cls
-    echo Starting server...
-    node ./bin/www
+    git fetch
+    git pull
+    if %errorlevel%==1 (
+        echo Repository has been updated.
+        echo Starting server...
+        node ./bin/www
+    ) else (
+        echo Repository is up to date.
+        echo Starting server...
+        node ./bin/www
+    )
 ) else (
     REM Internet is not accessible, do something else here
     echo Internet is not accessible.
