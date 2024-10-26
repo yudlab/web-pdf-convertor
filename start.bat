5fca065499575ccdcc2ef8c07c1453f40f54dffa
WintertonB <wtb@plp.local>
2024-10-26 17:45:14 +0100

    fix 2

--- start.bat
+++ start.bat
@@ -1,35 +1,43 @@
 @echo off
 title Heating the engine...
+
 taskkill>nul /f /im node.exe
 if "%1"=="" (
     title Web file convertor
-    echo Checking for updates...
-    echo Waiting for network availability...
-    ping -n 8 www.google.com >nul 2>&1
+    :wifiCheck
+    echo Checking Wi-Fi Connection...
+    netsh wlan show interfaces | findstr /i "State" | findstr /i "connected" >nul
     if %errorlevel%==0 (
-        git fetch>nul
-        git pull>nul
-        if %errorlevel%==1 (
+        echo Connected to network.
+        echo Checking for updates...
+        echo Waiting for network availability...
+        timeout>nul /t 2
+        nslookup www.google.com >nul 2>&1
+        if %errorlevel%==0 (
+            echo Updating repository...
+            git fetch
+            git pull
             start.bat --updated
             exit
         ) else (
-            echo Repository is up to date.
+            REM Internet is not accessible, do something else here
+            echo Internet is not accessible.
+            echo Update skipped.
             echo Starting server...
             npm run start
         )
     ) else (
-        REM Internet is not accessible, do something else here
-        echo Internet is not accessible.
-        echo Update skipped.
-        echo Starting server...
-        npm run start
+        echo An error occured.
+        echo It looks like you are not connected to a network.
+        echo Check your network connection and press any to continue...
+        pause
+        goto :wifiCheck
     )
 ) else (
     echo Repository updated...
     git log -1
-    timeout>nul /t 5
+    timeout>nul /t 2
     cls
     echo Starting server...
     npm run start
 )
-pause
