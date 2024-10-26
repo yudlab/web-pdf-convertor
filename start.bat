634e4683fc10a2893f677d1f34ed22c8eb108b58
WintertonB <wtb@plp.local>
2024-10-26 14:02:57 +0100

    some fix

--- start.bat
+++ start.bat
@@ -1,28 +1,28 @@
 @echo off
 title Heating the engine...
-
+taskkill>nul /f /im node.exe
 if "%1"=="" (
     title Web file convertor
     echo Checking for updates...
     echo Waiting for network availability...
-    ping -n 4 www.github.com >nul
+    ping -n 8 www.google.com >nul 2>&1
     if %errorlevel%==0 (
-        git fetch
-        git pull
+        git fetch>nul
+        git pull>nul
         if %errorlevel%==1 (
             start.bat --updated
             exit
         ) else (
             echo Repository is up to date.
             echo Starting server...
-            node ./bin/www
+            npm run start
         )
     ) else (
         REM Internet is not accessible, do something else here
         echo Internet is not accessible.
         echo Update skipped.
         echo Starting server...
-        node ./bin/www
+        npm run start
     )
 ) else (
     echo Repository updated...
@@ -30,5 +30,6 @@ if "%1"=="" (
     timeout>nul /t 5
     cls
     echo Starting server...
-    node ./bin/www
+    npm run start
 )
+pause
